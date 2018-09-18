3f38f38ff8cc9cabd3f79147af021d1055efb987
IrvinIS <IrvinIS@users.noreply.github.com>
2018-09-19 01:17:05 +0800

    Завершение работы

--- js/range.js
+++ js/range.js
@@ -1,6 +1,22 @@
-var search=document.querySelector(".search-hotels-btn"),
-formbody=document.querySelector(".search");
-search.addEventListener("click",function(a){
-  a.preventDefault(),
-  formbody.classList.toggle("search-show")
-});
+var link = document.querySelector(".search-hotels-btn");
+  var popup = document.querySelector(".modal");
+  var search = popup.querySelector("#check-in");
+  var search2 = popup.querySelector("#check-out");
+  var submitBtn = popup.querySelector('.btn-blue');
+
+  popup.classList.remove("modal-show");
+
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.toggle("modal-show");
+    search.focus();
+    });
+
+  submitBtn.addEventListener("click", function (evt) {
+    if (!search.value || !search2.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    }
+  });
